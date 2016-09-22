388688e9c39ecfb6746495c030c3df0787097717
ljy690 <657513865@qq.com>
2016-09-22 16:09:48 +0800

    廖瑾芸：配置过滤器，修复安全漏洞，可以防御 XSS 攻击，还可以防御 CSRF 攻击，SQL 注入等安全问题...

--- law/src/main/java/com/yc/law/handler/MessageHandler.java
+++ law/src/main/java/com/yc/law/handler/MessageHandler.java
@@ -12,11 +12,9 @@ import net.sf.json.JSONObject;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.request.WebRequest;
 
 import com.yc.law.entity.LeaveMsg;
 import com.yc.law.service.MessagesService;
